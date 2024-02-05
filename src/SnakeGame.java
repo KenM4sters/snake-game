5249c431558084e731fc1efbdca2c1099bd89a60
KenM4sters <EHCKenway@outlook.com>
2024-02-05 14:07:56 +0000

    snake controls complete

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -14,6 +14,9 @@ public class SnakeGame extends JPanel implements ActionListener {
     private final int cellSize;
     private static final int FRAME_RATE = 20;
     private boolean gameStarted = false;
+    private boolean gameOver = false;
+    private Direction direction = Direction.RIGHT;
+    private Direction newDirection = Direction.RIGHT;
     private final List<GamePoint> snake = new ArrayList<>();
     public SnakeGame(final int width, final int height) {
         super();
@@ -22,6 +25,10 @@ public class SnakeGame extends JPanel implements ActionListener {
         this.cellSize = width / (FRAME_RATE * 2);
         setPreferredSize(new Dimension(width, height));
         setBackground(Color.BLACK);
+
+        snake.addFirst(new GamePoint(500, 300));
+        snake.addFirst(new GamePoint(600, 300));
+        resetGameData();
     }
 
     public void startGame() {
@@ -31,17 +38,41 @@ public class SnakeGame extends JPanel implements ActionListener {
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    gameStarted = true;
-                }
+                handleKeyEvent(e);
             }
         });
         new Timer(1000 / FRAME_RATE, this).start();
     }
 
+    private void handleKeyEvent(final KeyEvent e) {
+        if(!gameStarted) {
+            if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+                gameStarted = true;
+            }
+        } else {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_UP:
+                    if(direction != Direction.DOWN)
+                        newDirection = Direction.UP;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != Direction.UP)
+                        newDirection = Direction.DOWN;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != Direction.LEFT)
+                        newDirection = Direction.RIGHT;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    if(direction != Direction.RIGHT)
+                        newDirection = Direction.LEFT;
+                    break;
+            }
+        }
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
-        resetGameData();
         super.paintComponent(g);
         if(!gameStarted) {
             g.setColor(Color.WHITE);
@@ -70,23 +101,46 @@ public class SnakeGame extends JPanel implements ActionListener {
         snake.add(new GamePoint(width / 2, height / 2));
     }
 
+    private void generateFood() {}
+
     private void moveSnake() {
         final GamePoint currentHead = snake.getFirst();
-        final GamePoint newHead = new GamePoint(currentHead.x + cellSize, currentHead.y);
+        final GamePoint newHead = switch(direction) {
+            case UP -> new GamePoint(currentHead.x, currentHead.y - cellSize);
+            case DOWN -> new GamePoint(currentHead.x, currentHead.y + cellSize);
+            case RIGHT -> new GamePoint(currentHead.x + cellSize, currentHead.y);
+            case LEFT -> new GamePoint(currentHead.x - cellSize, currentHead.y);
+        };
+
         snake.addFirst(newHead);
-        snake.removeLast();
-        for(final var point : snake) {
-            System.out.println(point.x);
+
+        if(checkCollision()) {
+            gameOver = true;
         }
+
+        direction = newDirection;
+        snake.removeLast();
+    }
+
+    private boolean checkCollision() {
+        final GamePoint head = snake.getFirst();
+        final var invalidWidth = (head.x < 0) || (head.x >= width - cellSize);
+        final var invalidHeight = (head.y < 0) || (head.y >= height - cellSize);
+        return (invalidWidth || invalidHeight);
+
     }
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-        if(gameStarted) {
+        if(gameStarted && !gameOver) {
+            System.out.println("playing");
             moveSnake();
         }
         repaint();
     }
 
     private record GamePoint(int x, int y) {}
+    private enum Direction {
+        UP, DOWN, RIGHT, LEFT
+    }
 }
